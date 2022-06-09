ae27e12002e295420c2ca160fd587d284f062261
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-09 17:32:06 +0300

    added to decimal conversion

--- compsci/data_representation.py
+++ compsci/data_representation.py
@@ -1,10 +1,15 @@
 bits = "0123456789ABCDEF"
 
-def convert_decimal(x, base):
+def from_decimal(x, base):
   if x == 0: return "0"
   if x < 0: x, base = -x, base * -1
   result = ""
   while x > 0:
     result = bits[x % base] + result
     x //= base
+  return result
+
+def to_decimal(x, base):
+  result = 0
+  for i, c in enumerate(x): result += bits.index(c) * (base ** (len(x) - i - 1))
   return result
