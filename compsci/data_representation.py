3145a1cec5391a59d679e4d090fb3fdefb5b7c13
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-09 19:09:33 +0300

    implemented binary to octet

--- compsci/data_representation.py
+++ compsci/data_representation.py
@@ -11,5 +11,12 @@ def from_decimal(x, base):
 
 def to_decimal(x, base):
   result = 0
-  for i, c in enumerate(x): result += bits.index(c) * (base ** (len(x) - i - 1))
+  for i, c in enumerate(x):result += bits.index(c) * (base ** (len(x) - i - 1))
   return result
+
+def from_binary(x, base): return from_decimal(to_decimal(x, 2), base)
+
+def binary_to_octet(x):
+  x = "".join(["0" * (len(x) % 3), x])
+  x = [list(x[i] for i in range(a * 3, 3 * (a + 1))) for a in range(len(x) // 3)]
+  return "".join([from_binary(a, 8) for a in x])
