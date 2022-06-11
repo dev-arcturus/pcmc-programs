99ccafe3813744a38180ada2932192dc796b24e8
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-11 10:04:51 +0300

    worked on implementing float conversions

--- compsci/data_representation.py
+++ compsci/data_representation.py
@@ -5,21 +5,23 @@ def safe_get_index(array, search):
   except(ValueError): return None
 
 def calculate_offset(x):
-  return (type(x) == "float" and -1 * (len(str(x)) - 1 - safe_get_index([_ for _ in str(x)], "."))) or 0
+  return ("." in str(x) and -1 * (len(str(x)) - 1 - safe_get_index([_ for _ in str(x)], "."))) or 0
+
+def break_up_decimal(x): return [int(z) for z in str(x).split(".")[:2]] if "." in str(x) else [x, 0]
 
 def from_decimal(x, base):
   if x == 0: return "0"
   if x < 0: x, base = -x, base * -1
-  result, x = "", int(str(x).replace(".", ""))
-  while x > 0: result, x = bits[x % base] + result, x // base
-  return result
+  [a, b], [x, y], is_float = ["", ""], break_up_decimal(x) , "." in str(x)
+  while x > 0: a, x = bits[x % base] + a, x // base
+  y = float(f"0.{y}")
+  while y < base - 1 and y != 0: b, y = b + str(bits[int(y % base)]), y * base 
+  return f"{a}.{b}" if is_float else a
 
 def to_decimal(x, base):
   result, offset, x = 0, calculate_offset(x), str(x).replace(".", "")
   for i, c in enumerate(x): result += bits.index(c) * (base ** (len(x) - i + offset - 1))
   return result
-
-print(to_decimal("1010.01", 2))
 
 def convert(x, current_base, target_base): return from_decimal(to_decimal(x, current_base), target_base)
 
