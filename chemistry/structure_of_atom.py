ccfa4276174ac2dc292f01eea4ed2b7ae45b26ba
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-15 21:00:46 +0300

    implemented wavelength and frequency

--- chemistry/structure_of_atom.py
+++ chemistry/structure_of_atom.py
@@ -1,3 +1,9 @@
 BOHRS = 52.9 * 10 ** (-12)
 SPEED_OF_LIGHT = 3 * 10 ** 8
 PLANKS = 6.626 * 10 ** (-34)
+
+def get_wavelength(frequency):
+    return SPEED_OF_LIGHT / frequency
+
+def get_frequency(wavelength):
+    return SPEED_OF_LIGHT / wavelength
