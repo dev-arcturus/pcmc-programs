88abba6345a909a1fc7faa9dbba0bf22c4e21dcb
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-17 13:32:17 +0300

    made code concise

--- physics/motion_in_a_plane.py
+++ physics/motion_in_a_plane.py
@@ -1,9 +1,10 @@
-from math import sqrt, atan, cos
+from math import degrees, sin, sqrt, atan, cos
 from functools import reduce
 
 
 class Vector():
   def __init__(self, x=0, y=0, z=0):
+    if x == 0: raise Exception('x cannot be 0')
     self.x = x
     self.y = y
     self.z = z
@@ -18,24 +19,16 @@ class Vector():
     return atan(self.y / self.x)
 
   def __add__(self, other):
-    x = self.x + other.x
-    y = self.y + other.y
-    return [x, y]
+    return [self.x + other.x, self.y + other.y]
 
   def __sub__(self, other):
-    x = self.x - other.x
-    y = self.y - other.y
-    return [x, y]
+    return [self.x - other.x, self.y - other.y]
   
   def __mul__(self, other):
-    x = self.x * other.x
-    y = self.y * other.y
-    return [x, y]
+    return [self.x * other.x, self.y * other.y]
   
   def __truediv__(self, other):
-    x= self.x / other.x
-    y = self.y / other.y
-    return [x, y]
+    return [self.x / other.x, self.y / other.y]
   
   def __str__(self):
     [x, y, z] = [self.x, self.y, self.z]
@@ -50,3 +43,9 @@ class Vector():
 def get_magnitude_of_resultant_vector(A: Vector, B: Vector):
   mA, mB = [x.get_magnitude() for x in [A, B]]
   return round(sqrt(mA ** 2 + mB ** 2 + 2 * mA * mB * cos(B.get_theta())), 2)
+
+def get_alpha(A: Vector, B: Vector):
+  mA, mB, theta = [*[x.get_magnitude() for x in [A, B]], B.get_theta()]
+  return degrees(atan(mB * sin(theta) / (mA + mB * cos(theta))))
+
+print(get_alpha(Vector(1, 1), Vector(2, 1)))
