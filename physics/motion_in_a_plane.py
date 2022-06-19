5e57485dca4ea66055909d7f82fb3c48e3961e5b
arcturus <64982590+dev-arcturus@users.noreply.github.com>
2022-06-19 12:02:40 +0300

    removed exception

--- physics/motion_in_a_plane.py
+++ physics/motion_in_a_plane.py
@@ -4,10 +4,7 @@ from functools import reduce
 
 class Vector():
   def __init__(self, x=0, y=0, z=0):
-    if x == 0: raise Exception('x cannot be 0')
-    self.x = x
-    self.y = y
-    self.z = z
+    self.x, self.y, self.z = x, y, z
 
   def scalar(self):
     return [abs(self.x), abs(self.y), abs(self.z)]
@@ -16,10 +13,20 @@ class Vector():
     return self.__repr__()
   
   def get_magnitude(self):
-    return round(sqrt(reduce(lambda x, y: x + y ** 2, [self.x, self.y, self.z])), 2)
+    return round(sqrt(reduce(lambda x, y: x + y ** 2, [0, self.x, self.y, self.z])), 2)
 
   def get_theta(self):
     return atan(self.y / self.x)
+
+  def __str__(self):
+    [x, y, z] = [self.x, self.y, self.z]
+    if z == 0: return f'({x}, {y})'
+    return f'({x}, {y}, {z})' 
+
+  def __repr__(self):
+    [x, y, z] = [self.x, self.y, self.z]
+    if z == 0: return f'{x}î + {y}ĵ'
+    return f'{x}î + {y}ĵ + {z}k̂'
 
   def __add__(self, other):
     return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
@@ -32,16 +39,6 @@ class Vector():
   
   def __truediv__(self, other):
     return Vector(self.x / other.x, self.y / other.y, self.z / other.z)
-  
-  def __str__(self):
-    [x, y, z] = [self.x, self.y, self.z]
-    if z == 0: return f'({x}, {y})'
-    return f'({x}, {y}, {z})' 
-
-  def __repr__(self):
-    [x, y, z] = [self.x, self.y, self.z]
-    if z == 0: return f'{x}î + {y}ĵ'
-    return f'{x}î + {y}ĵ + {z}k̂'
 
 def get_magnitude_of_resultant_vector(A: Vector, B: Vector):
   mA, mB = [x.get_magnitude() for x in [A, B]]
@@ -51,4 +48,4 @@ def get_alpha(A: Vector, B: Vector):
   mA, mB, theta = [*[x.get_magnitude() for x in [A, B]], B.get_theta()]
   return degrees(atan(mB * sin(theta) / (mA + mB * cos(theta))))
 
-print(get_alpha(Vector(1, 1), Vector(2, 1)))
+
